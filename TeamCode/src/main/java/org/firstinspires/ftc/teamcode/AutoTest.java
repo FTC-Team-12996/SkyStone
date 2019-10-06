1a56517beac420472210ff452f5a809db3fc90b4
Andy Greer <andersonagme@gmail.com>
2019-10-06 16:56:15 -0400

    Create AutoTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous
+
+// One rotation of the mecanum wheels is 12 inches
+// One rotation of the mecanum wheels is 1440 ticks
+
+public class AutoTest extends LinearOpMode {
+    private DcMotor leftFront;
+    private DcMotor rightFront;
+    private DcMotor leftBack;
+    private DcMotor rightBack;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        driveForward(5);
+    }
+
+    public void driveForward(double inches) {
+        // left is positive while right is negative
+        // rightBack has an encoder
+        int ticks = (int)(inches * 120);
+
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // Torquenado has 1440 ticks
+
+        rightBack.setTargetPosition(ticks);
+
+        leftFront.setPower(1);
+        rightFront.setPower(-1);
+        leftBack.setPower(1);
+        rightBack.setPower(-1);
+
+
+        rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        while(rightBack.isBusy()) {
+            // wait
+        }
+
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+
+    }
+}
