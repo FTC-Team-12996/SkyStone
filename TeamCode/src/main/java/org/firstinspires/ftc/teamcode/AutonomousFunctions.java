d4ca7370faaa95379c9fe3dbc43e2a0e36f2cf7f
Noah-Something <43120746+Noah-Something@users.noreply.github.com>
2019-10-27 17:33:49 -0400

    ok

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousFunctions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousFunctions.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import static java.lang.Thread.sleep;
 
 public class AutonomousFunctions implements Autonomous_Interface {
     private DcMotor leftFront;
@@ -15,6 +16,7 @@ public class AutonomousFunctions implements Autonomous_Interface {
 
     boolean ClawMax = false;
     boolean ClawMin = false;
+    boolean stoptime = false;
 
     final int slideMax = 2280;
 
@@ -31,131 +33,239 @@ public class AutonomousFunctions implements Autonomous_Interface {
         rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
-    public void driveForward(double power, int TargetPosition, long timeMs) {
-        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    //Old Code (Moving)
+    //leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    //rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    //leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    //rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+    //leftFront.setTargetPosition(-TargetPosition);
+    //rightFront.setTargetPosition(-TargetPosition);
+    //leftBack.setTargetPosition(-TargetPosition);
+    //rightBack.setTargetPosition(-TargetPosition);
+
+        //leftFront.setPower(power);
+        //rightFront.setPower(-power);
+        //leftBack.setPower(power);
+        //rightBack.setPower(-power);
+
+
+    //leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    //rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    //leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    //rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+    //int currentMotorPosition = rightBack.getCurrentPosition();
+    //    if (currentMotorPosition <= -TargetPosition)
+    //{
+        //leftFront.setPower(0);
+        //rightFront.setPower(0);
+        //leftBack.setPower(0);
+        //rightBack.setPower(0);
+    //}
+
+    while(gamepad2.left_stick_y > 0.1){
+        int currentSlidePosition = slide.getCurrentPosition();
+        if (currentSlidePosition >= slideMax)
+        {
+            slide.setPower(0);
+        }
+        else {
+            raiseSlide(0.1);
+        }
+        stoptime = true;
+    }
+
+    while(gamepad2.left_stick_y < 0.1){
+        int currentSlidePosition = slide.getCurrentPosition();
+        if (currentSlidePosition <= slideMax)
+        {
+            slide.setPower(0);
+        }
+        else {
+            lowerSlide(0.1);
+        }
+        stoptime = true;
+    }
+
+    if(stoptime = true) {
+        stoptime = false;
+        sleep(100);
+    }
+
+    public void lowerSlide(double power) {
+        slide.setPower(-power);
+    }
+
+    public void raiseSlide(double power) {
+        slide.setPower(power);
+    }
+
+    @Override
+    public void driveForward(double inches, double power) {
+     int ticks = (int)(inches * 60);
+
+     rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+     rightBack.setTargetPosition(ticks);
+
+     leftFront.setPower(-power);
+     rightFront.setPower(power);
+     leftBack.setPower(-power);
+     rightBack.setPower(power);
+
+     rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+     while(rightBack.isBusy()) {
+            //Nothing
+     }
+
+     leftFront.setPower(0);
+     rightFront.setPower(0);
+     leftBack.setPower(0);
+     rightBack.setPower(0);
+
+    }
+
+    @Override
+    public void driveBack(double inches, double power) {
+        int ticks = (int)(inches * 60);
+
         rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        leftFront.setTargetPosition(TargetPosition);
-        rightFront.setTargetPosition(TargetPosition);
-        leftBack.setTargetPosition(TargetPosition);
-        rightBack.setTargetPosition(TargetPosition);
+        rightBack.setTargetPosition(-ticks);
+
+        leftFront.setPower(power);
+        rightFront.setPower(-power);
+        leftBack.setPower(power);
+        rightBack.setPower(-power);
+
+        rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        while(rightBack.isBusy()) {
+
+        }
+
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+
+    }
+
+    @Override
+    public void turnRight(double inches, double power) {
+        int ticks = (int)(inches * 60);
+
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        rightBack.setTargetPosition(ticks);
 
         leftFront.setPower(power);
         rightFront.setPower(power);
         leftBack.setPower(power);
         rightBack.setPower(power);
 
-        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        try {
-            Thread.sleep(timeMs);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        while(rightBack.isBusy()) {
+
         }
+
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+
+
 
     }
 
     @Override
-    public void driveBack(double power, int TargetPosition, long timeMs) {
-        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    public void turnLeft(double inches, double power) {
+        int ticks = (int)(inches * 60);
+
         rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        leftFront.setTargetPosition(-TargetPosition);
-        rightFront.setTargetPosition(-TargetPosition);
-        leftBack.setTargetPosition(-TargetPosition);
-        rightBack.setTargetPosition(-TargetPosition);
+        rightBack.setTargetPosition(ticks);
 
         leftFront.setPower(-power);
         rightFront.setPower(-power);
         leftBack.setPower(-power);
         rightBack.setPower(-power);
 
-
-        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        try {
-            Thread.sleep(timeMs);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        while(rightBack.isBusy()) {
+
         }
+
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+
     }
 
+
     @Override
-    public void turnRight(double power, int TargetPosition, long timeMs) {
-        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    public void moveRight(double inches, double power) {
+        int ticks = (int)(inches * 60);
+
         rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        leftFront.setTargetPosition(TargetPosition);
-        rightFront.setTargetPosition(-TargetPosition);
-        leftBack.setTargetPosition(TargetPosition);
-        rightBack.setTargetPosition(-TargetPosition);
+        rightBack.setTargetPosition(ticks);
 
         leftFront.setPower(power);
-        rightFront.setPower(-power);
-        leftBack.setPower(power);
+        rightFront.setPower(power);
+        leftBack.setPower(-power);
         rightBack.setPower(-power);
 
-
-        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        try {
-            Thread.sleep(timeMs);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        while(rightBack.isBusy()) {
+
         }
+
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+
     }
 
     @Override
-    public void turnLeft(double power, int TargetPosition, long timeMs) {
-        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    public void moveLeft(double inches, double power) {
+        int ticks = (int)(inches * 60);
+
         rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        leftFront.setTargetPosition(-TargetPosition);
-        rightFront.setTargetPosition(TargetPosition);
-        leftBack.setTargetPosition(-TargetPosition);
-        rightBack.setTargetPosition(TargetPosition);
+        rightBack.setTargetPosition(ticks);
 
         leftFront.setPower(-power);
-        rightFront.setPower(power);
-        leftBack.setPower(-power);
+        rightFront.setPower(-power);
+        leftBack.setPower(power);
         rightBack.setPower(power);
 
-
-        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        try {
-            Thread.sleep(timeMs);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        while(rightBack.isBusy()) {
+
         }
+
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+
     }
 
     @Override
     public void raiseClaw(double power, int TargetPosition, long timeMs) {
         int currentSlidePosition = slide.getCurrentPosition();
-        if ((currentSlidePosition + (TargetPosition - currentSlidePosition)) > slideMax)
+        if ((currentSlidePosition + (TargetPosition - currentSlidePosition) > slideMax))
         {
-            //Do Nothing
+            slide.setPower(0);
         }
         else
             slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -171,9 +281,9 @@ public class AutonomousFunctions implements Autonomous_Interface {
     @Override
     public void lowerClaw(double power, int TargetPosition, long timeMs) {
         int currentSlidePosition = slide.getCurrentPosition();
-        if ((currentSlidePosition + (TargetPosition - currentSlidePosition)) < 0)
+        if ((currentSlidePosition + (TargetPosition - currentSlidePosition) < 0))
         {
-            //Do Nothing
+            slide.setPower(0);
         }
         else
             slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Interface.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Interface.java
@@ -1,13 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
 public interface Autonomous_Interface {
-    public void driveForward(double power, int TargetPosition, long timeMs);
+    public void driveForward(double inches, double power);
 
-    public void driveBack(double power, int TargetPosition, long timeMs);
+    public void driveBack(double inches, double power);
 
-    public void turnRight(double power, int TargetPosition, long timeMs);
+    public void turnRight(double inches, double power);
 
-    public void turnLeft(double power, int TargetPosition, long timeMs);
+    public void turnLeft(double inches, double power);
+
+    public void moveRight(double inches, double power);
+
+    public void moveLeft(double inches, double power);
 
     public void raiseClaw(double power, int TargetPosition, long timeMs);
 
