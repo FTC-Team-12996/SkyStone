4154eeafbaa2f48b0647013d298cb3dc7673519e
Andy Greer <andersonagme@gmail.com>
2019-11-10 17:34:54 -0500

    Commented and updated autonomous programs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUTO_BLUE_Stone_Collection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUTO_BLUE_Stone_Collection.java
@@ -1,3 +1,5 @@
+// Autonomous code developed by Andy Greer during the 2019 season
+
 package org.firstinspires.ftc.teamcode;
 
 import android.app.Activity;
@@ -27,54 +29,64 @@ public class AUTO_BLUE_Stone_Collection extends LinearOpMode {
         sensorDistance = hardwareMap.get(DistanceSensor.class, "colorSensor");
         int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
         final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+        clawServo = hardwareMap.get(Servo.class, "clawServo");
 
         waitForStart();
 
         while(opModeIsActive()) {
-            clawServo.setPosition(0);
+            clawServo.setPosition(0); // Resets the servo
 
-            while(Double.isNaN(sensorDistance.getDistance(DistanceUnit.CM))) {
+            while(Double.isNaN(sensorDistance.getDistance(DistanceUnit.CM))) { // Drives until the stones are in view of the distance sensor
                 driveBase.driveForward(0.5);
             }
 
-            while(sensorDistance.getDistance(DistanceUnit.CM) > 1) {
+            while(sensorDistance.getDistance(DistanceUnit.CM) > 1) { // Drives slowly up next to the first stone
                 driveBase.driveForward(0.1);
             }
 
             driveBase.stopMotors();
 
-            while(!skystoneIsDetected) {
-                for (int x = 0; x < 6; x++) {
-                    for (int i = 0; i < 100; i++) {
-                        alphaAvrg += colorsensor.alpha();
-                        sleep(10);
-                    }
 
-                    if (alphaAvrg < 400) {
-                        clawServo.setPosition(1);
-                        skystoneIsDetected = true;
-                    } else {
-                        driveBase.moveRight(5, 0.3);
-                    }
-                    alphaAvrg = 0;
+            // If the skystone is detected, it is picked up and the loop ends
+            // If it's not, the robot moves to the next stone and tries again
+            for (int x = 0; x < 5; x++) {
+                for (int i = 0; i < 100; i++) { // Takes the average of 100 measurements from the color sensor
+                    alphaAvrg += colorsensor.alpha();
+                    sleep(10);
                 }
+
+                if (alphaAvrg < 400) {
+                    clawServo.setPosition(1);
+                    skystoneIsDetected = true;
+                    break;
+                } else {
+                    driveBase.moveRight(5, 0.3);
+                }
+                alphaAvrg = 0;
+            }
+
+            if(!skystoneIsDetected) { // Grabs the last stone if the others were not identified as the skystone
+                clawServo.setPosition(1);
+                skystoneIsDetected = true;
+                driveBase.moveRight(5, 0.3);
+
             }
         }
 
-        driveBase.driveBackwards(10, 1);
+        driveBase.driveBackwards(10, 1); // Backs away from the skystone
 
-        sleep(100);
+        sleep(100); // Pause for gear safety
 
-        driveBase.moveLeft(60, 1);
+        driveBase.moveLeft(60, 1); // Moves over the midline
 
-        clawServo.setPosition(0);
+        clawServo.setPosition(0); // Drops the skystone
 
-        driveBase.moveRight(20, 1);
+        driveBase.moveRight(20, 1); // Moves back onto the midline
 
-        sleep(100);
+        sleep(100); // Pause for gear safety
 
-        driveBase.turnLeft(3, 1);
+        driveBase.turnLeft(3, 1); // Turns to maximize size of the robot over midline
 
-        clawServo.setPosition(0.5);
+        clawServo.setPosition(0.5); // Lowers the claw to maximize size
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUTO_RED_Stone_Collection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUTO_RED_Stone_Collection.java
@@ -1,3 +1,5 @@
+// Autonomous code developed by Andy Greer during the 2019 season
+
 package org.firstinspires.ftc.teamcode;
 
 import android.app.Activity;
@@ -27,54 +29,61 @@ public class AUTO_RED_Stone_Collection extends LinearOpMode {
         sensorDistance = hardwareMap.get(DistanceSensor.class, "colorSensor");
         int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
         final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+        clawServo = hardwareMap.get(Servo.class, "clawServo");
 
         waitForStart();
 
-        while(opModeIsActive()) {
-            clawServo.setPosition(0);
+        while (opModeIsActive()) {
+            clawServo.setPosition(0); // Resets the servo
 
-            while(Double.isNaN(sensorDistance.getDistance(DistanceUnit.CM))) {
+            while (Double.isNaN(sensorDistance.getDistance(DistanceUnit.CM))) { // Drives forwards while the stones are out of sight
                 driveBase.driveForward(0.5);
             }
 
-            while(sensorDistance.getDistance(DistanceUnit.CM) > 1) {
+            while (sensorDistance.getDistance(DistanceUnit.CM) > 1) { // Drives up to the first stone
                 driveBase.driveForward(0.1);
             }
 
             driveBase.stopMotors();
 
-            while(!skystoneIsDetected) {
-                for (int x = 0; x < 6; x++) {
-                    for (int i = 0; i < 100; i++) {
-                        alphaAvrg += colorsensor.alpha();
-                        sleep(10);
-                    }
-
-                    if (alphaAvrg < 400) {
-                        clawServo.setPosition(1);
-                        skystoneIsDetected = true;
-                    } else {
-                        driveBase.moveLeft(5, 0.3);
-                    }
-                    alphaAvrg = 0;
+            // If the skystone is detected, it is picked up and the loop ends
+            // If it's not, the robot moves to the next stone and tries again
+            for (int x = 0; x < 5; x++) {
+                for (int i = 0; i < 100; i++) {
+                    alphaAvrg += colorsensor.alpha();
+                    sleep(10);
                 }
+                if (alphaAvrg < 400) {
+                    clawServo.setPosition(1);
+                    skystoneIsDetected = true;
+                    break;
+                } else {
+                    driveBase.moveLeft(5, 0.3);
+                }
+                alphaAvrg = 0;
             }
+
+            if(!skystoneIsDetected) { // Grabs the leftover stone if none have been grabbed
+                clawServo.setPosition(1);
+                skystoneIsDetected = true;
+                driveBase.moveLeft(5, 0.3);
+            }
+
+            driveBase.driveBackwards(10, 1); // Backs away from the stones
+
+            sleep(100); // Pauses for gear safety
+
+            driveBase.moveRight(60, 1); // Moves across the midline
+
+            clawServo.setPosition(0); // Drops the skystone
+
+            driveBase.moveLeft(20, 1); // Moves back onto the midline
+
+            sleep(100); // Pauses for gear safety
+
+            driveBase.turnRight(3, 1); // Turns to maximize size of the robot over midline
+
+            clawServo.setPosition(0.5); // Drops the servo to maximize size of the robot
         }
-
-        driveBase.driveBackwards(10, 1);
-
-        sleep(100);
-
-        driveBase.moveRight(60, 1);
-
-        clawServo.setPosition(0);
-
-        driveBase.moveLeft(20, 1);
-
-        sleep(100);
-
-        driveBase.turnRight(3, 1);
-
-        clawServo.setPosition(0.5);
     }
 }
